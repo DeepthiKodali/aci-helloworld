fcd13c96b94edbff64d20989d5eeaf6b606ab414
Deepthi  Kodali <deepthi.kodali@prodapt.com>
2024-02-22 19:03:16 +0000

    feat: modified ports

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,2 @@
-var expect = require('chai').expect;
-var request = require('request');
-describe('app testing', () => {
-  it('/ status code', (done) => {
-    request('http://localhost:8080/', (err, result, body) => {
-    expect(result.statusCode).to.equal(200);
-    expect(body).to.equal(JSON.stringify({"message":"Welcome to Azure Container Instances!"}));
-    done();
-  });
- });
+test('Check addTodo able add todo to todoList', () => {
 });
