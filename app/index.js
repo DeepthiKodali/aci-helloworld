078bbb01dde4887b0337e0e957c9935692ee959a
Deepthi  Kodali <deepthi.kodali@prodapt.com>
2024-02-22 19:37:09 +0000

    feat: modified ports

--- app/index.js
+++ app/index.js
@@ -12,4 +12,4 @@ app.get('/', (req, res) => {
 var listener = app.listen(process.env.PORT || 80, function() {
  console.log('listening on port ' + listener.address().port);
 });
-module.exports = app;
+//module.exports = app;
